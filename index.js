a5236bc5105b8099e7d9d33a3bfca4b736bf782b
seb-art <kimanzikims300@gmail.com>
2023-09-20 13:57:40 +0300

    implemented the movie genres

--- index.js
+++ index.js
@@ -28,11 +28,63 @@ app.get("/api/genres", (req, res) => {
 app.get("/api/genres/:id", (req, res) => {
   const genre = genres.find((g) => g.id === parseInt(req.params.id));
   if (!genre) {
-    return res.status(404).send("The movie with the given ID was not found.");
+    return res
+      .status(404)
+      .send("The movie-Genre with the given ID was not found.");
   }
   res.send(genre);
 });
 
+//POST request
+app.post("/api/genres", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genre);
+});
+
+//PUT request
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) {
+    return res
+      .status(404)
+      .send("The movie-Genre with the given ID was not found.");
+  }
+  const { error } = validateGenre(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+//DELETE request
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre) {
+    res.status(404).send("The genre with the Given ID was not found");
+  }
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send(genre);
+});
+
+//INPUT VALIDATION
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+
+  return Joi.validate(genre, schema);
+}
+// SERVING
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
