022cf6cd3f5beecc6f12ea9ef69e14725d7f8eb8
seb-art <kimanzikims300@gmail.com>
2024-06-01 21:42:21 +0300

    restructured the customers

---
+++ controllers/movieController.js
@@ -0,0 +1,79 @@
+const Joi = require("joi");
+
+const getMovies = async (req, res) => {
+  const movies = await Movie.find();
+  res.send(movies);
+};
+
+const getMovie = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).send("Invalid ID format");
+  }
+  const movie = await Movie.findById(req.params.id);
+  if (!movie) {
+    return res
+      .status(404)
+      .send("The movie-Genre with the given ID was not found.");
+  }
+  res.send(movie);
+};
+
+const createMovie = async (req, res) => {
+  const { error } = validateMovie(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let movie = new Movie({ name: req.body.name, genre: req.body.genre });
+
+  res.send(movie);
+};
+
+const updateMovie = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).send("Invalid ID format");
+  }
+  const { error } = validateMovie(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    { name: req.params.name },
+    { new: true }
+  );
+  if (!movie) {
+    return res
+      .status(404)
+      .json({ message: "The movie-Genre with the given ID was not found." });
+  }
+  res.send(movie);
+};
+
+const deleteMovie = async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    return res.status(400).send("Invalid ID format");
+  }
+  const movie = await Movie.findByIdAndDelete(req.params.id);
+  if (!movie) {
+    res.status(404).send("The genre with the Given ID was not found");
+  }
+
+  res.send(movie);
+};
+
+//INPUT VALIDATION
+function validateMovie(movie) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(movie);
+}
+
+module.exports = {
+  getMovies,
+  getMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie,
+};
--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
-const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
+const {
+  getMovies,
+  getMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie,
+} = require("../controllers/movieController");
 
 const movieSchema = new mongoose.Schema({
   name: {
@@ -22,77 +28,17 @@ const Movie = mongoose.model("Movie", movieSchema);
 
 //GET request
 
-router.get("/", async (req, res) => {
-  const movies = await Movie.find();
-  res.send(movies);
-});
+router.get("/", getMovies);
 
-router.get("/:id", async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).send("Invalid ID format");
-  }
-  const movie = await Movie.findById(req.params.id);
-  if (!movie) {
-    return res
-      .status(404)
-      .send("The movie-Genre with the given ID was not found.");
-  }
-  res.send(movie);
-});
+router.get("/:id", getMovie);
 
 //POST request
-router.post("/", async (req, res) => {
-  const { error } = validateMovie(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  let movie = new Movie({ name: req.body.name, genre: req.body.genre });
-
-  res.send(movie);
-});
+router.post("/", createMovie);
 
 //PUT request
-router.put("/:id", async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).send("Invalid ID format");
-  }
-  const { error } = validateMovie(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  const movie = await Movie.findByIdAndUpdate(
-    req.params.id,
-    { name: req.params.name },
-    { new: true }
-  );
-  if (!movie) {
-    return res
-      .status(404)
-      .json({ message: "The movie-Genre with the given ID was not found." });
-  }
-  res.send(movie);
-});
+router.put("/:id", updateMovie);
 
 //DELETE request
-router.delete("/:id", async (req, res) => {
-  if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).send("Invalid ID format");
-  }
-  const movie = await Movie.findByIdAndDelete(req.params.id);
-  if (!movie) {
-    res.status(404).send("The genre with the Given ID was not found");
-  }
-
-  res.send(movie);
-});
-
-//INPUT VALIDATION
-function validateMovie(movie) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  return schema.validate(movie);
-}
+router.delete("/:id", deleteMovie);
 
 module.exports = router;
